ef716a196f00ba414eb98fce26a8bb139a9b1189
Aifuwa Clinton <clintonaifuwa7@gmail.com>
2026-09-13 07:41:01 -0700

    changes made

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
       {/* Background video */}
       <video
       ref={videoRef}
-       src={assets.ExteriorVideo}
+       src={assets.InteriorVideo}
         autoPlay
         muted
         loop
